2de8487e68fdbf61b141ea7b2b68735018db8166
Carlo Bruno <carlofbruno@gmail.com>
2019-03-01 14:02:06 -0800

    completed code along

---
+++ config/passportConfig.js
@@ -0,0 +1,45 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("../models");
+
+/**
+ * @param cb -> callback, internal function from passport
+ * @param user -> model,
+ * */
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.user
+    .findById(id)
+    .then(function(user) {
+      cb(null, user);
+    })
+    .catch(cb);
+});
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password"
+    },
+    function(email, password, cb) {
+      db.user
+        .findOne({
+          where: { email: email }
+        })
+        .then(function(user) {
+          if (!user || !user.validPassword(password)) {
+            cb(null, false);
+          } else {
+            cb(null, user);
+          }
+        })
+        .catch(cb);
+    }
+  )
+);
+
+module.exports = passport;
